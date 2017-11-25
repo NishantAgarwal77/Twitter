78e17389eba406508691e45bb8245bbe7ae0da34
Moulik Agarwal <moulikagarwal@Mouliks-MacBook-Air.local>
2017-11-25 14:19:23 -0500

    Add login and register api at server and client

--- lib/twitterClient.ex
+++ lib/twitterClient.ex
@@ -10,4 +10,32 @@ defmodule TwitterClient do
         state = %{}
         {:ok, state}
     end
+
+    def register_client(username,password) do
+        server_state = GenServer.call(String.to_atom("twitterServer"),{:get_state, "twitterServer"})
+        user_state = Map.get(server_state,username)
+        if(Map.get(user_state,username) == nil) do
+            GenServer.call(String.to_atom("twitterServer"),{:register_user, {username,password}}) 
+            IO.puts "Successfully registered user"
+        else
+            IO.puts "Username already exists"
+        end 
+    end
+
+    def login_client(username,password) do
+        server_state = GenServer.call(String.to_atom("twitterServer"),{:get_state, "twitterServer"})
+        user_state = Map.get(server_state,"users")
+        if(user_state == nil) do
+            IO.puts "User Not found .. Registering user"
+            register_client(username,password)
+        else
+            if(Map.get(user_state,"password")==password) do
+                #GenServer.start_link(__MODULE__, {user_name,pass_word,{}}, name: String.to_atom(user_name))
+                GenServer.call(String.to_atom("twitterServer"),{:set_active_user,username})
+                IO.puts "Login Successful"
+            else
+                IO.puts "Sorry Wrong Password !!"
+            end
+        end
+    end
 end
--- lib/twitterServer.ex
+++ lib/twitterServer.ex
@@ -10,4 +10,24 @@ defmodule TwitterServer do
         state = %{}
         {:ok, state}
     end
+
+    def handle_call({:register ,user_contact},_from,state) do 
+        username = elem(user_contact,0)
+        password = elem(user_contact,1)
+        user_state=Map.get(state,username)
+        registered_user = %{"username" => username, "password" => password, "tweets" => [], "following"=>[],"status" => "active"}}
+        state = Map.put(state,username,registered_user)
+        {:reply,state,state}
+   end
+
+   def handle_call({:set_active_user ,username},_from,state) do  
+        user_state=Map.get(state,username)
+        user_state=Map.put(user_state,"status","active")
+        state=Map.put(state,username,user_state)
+        {:reply,state,state}
+   end 
+
+   def handle_call({:get_state ,new_message},_from,state) do  
+       {:reply,state,state}
+   end
 end
