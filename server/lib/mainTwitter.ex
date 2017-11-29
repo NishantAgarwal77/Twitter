5baaf68a7355d8efdd39ff24cf1d562634ae879d
Moulik Agarwal <moulikagarwal@Mouliks-MacBook-Air.local>
2017-11-28 22:02:59 -0500

    beam changes

--- server/lib/mainTwitter.ex
+++ server/lib/mainTwitter.ex
@@ -21,7 +21,7 @@ defmodule MainTwitter do
         receive do
             {:startServer} -> TwitterServerSupervisor.start_link(serverIP)                       
                 #startTwitterServer(sender)                
-                :timer.sleep(50000)
+                IO.gets("")
                 send sender, {:serverTerminate} 
             {:terminateServer} -> send sender, {:serverTerminate}                                     
         end    
